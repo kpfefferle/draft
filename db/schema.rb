9c1359a881e0b76c6e609f63a9ec05862ac08d15
Kevin Pfefferle <kevin@kevinpfefferle.com>
2014-07-13 17:00:20 -0400

    Create Player model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140713201225) do
+ActiveRecord::Schema.define(version: 20140713205652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20140713201225) do
     t.integer  "round"
     t.integer  "order"
     t.integer  "player_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "players", force: true do |t|
+    t.string   "name"
+    t.string   "position"
     t.datetime "created_at"
     t.datetime "updated_at"
   end

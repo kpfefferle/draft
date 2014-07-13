433e94119523ce8c672bc9e87ac96d77efde624f
Kevin Pfefferle <kevin@kevinpfefferle.com>
2014-07-13 16:52:15 -0400

    Add task to seed DB with Picks

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,33 @@
 public
 
+  def csv_lines(file)
+    csv_path = File.join(Rails.root, 'db', 'seeds', "#{file}.csv")
+    lines = File.open(csv_path).readlines
+    header = lines.shift
+    lines
+  end
+
   def clean_string(string)
     string.to_s.gsub!(/\A"|"\Z/, '')
     string.strip
   end
 
 Team.destroy_all
-teams_path = File.join(Rails.root, 'db', 'seeds', 'teams.csv')
-team_lines = File.open(teams_path).readlines
-header = team_lines.shift
-team_lines.each do |line|
+csv_lines('teams').each do |line|
   values = line.strip.split(',')
   Team.create({
     :name => clean_string(values[0]),
     :division => clean_string(values[1])
+  })
+end
+
+Pick.destroy_all
+csv_lines('order').each do |line|
+  values = line.strip.split(',')
+  team = Team.find_by_name(clean_string(values[2]))
+  Pick.create({
+    :team => team,
+    :round => values[0].to_i,
+    :order => values[1].to_i
   })
 end
